c249f58034c61bb2098248ad8a3cc85832e8ab4c
Raven <allforgames123456@gmail.com>
2020-08-11 23:31:35 +0530

    js final fix.

--- Submissions/module4_solution/js/script.js
+++ Submissions/module4_solution/js/script.js
@@ -2,12 +2,12 @@
 	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 
-for (var i=0; i<names.length; i++  ) {
+for (var i=0; i< names.length; i++  ) {
 	
 	var arr_name = names[i];
 	var firstLetter =arr_name.charAt(0).toLowerCase();
 
-  if (firstLetter == "j") {
+  if (firstLetter === 'j') {
    byeSpeaker.speak(names[i]);
   } 
   else {
